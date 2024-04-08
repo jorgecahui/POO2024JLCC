4404d24d64d5f8dcc22e026a506e5df0bc00e488
rosmeryhanccoch <rosmerysistemas11@gmail.com>
2024-04-08 10:19:54 -0500

    Agregando archivo

---
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/polimorfismo/Loro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.polimorfismo;
+
+/**
+ *
+ * @author Lab-IoT
+ */
+public class Loro  extends Animal{
+    
+    @Override
+    public void sonidoAnimal (){
+        System.out.println("hola manito aprende ....aprende ...esfuerzate");
+    }
+    
+}
