5695c6539be6a973d1f043618b269fd515bb2dd8
rosmeryhanccoch <rosmerysistemas11@gmail.com>
2024-04-29 10:01:12 -0500

    agreg trabajo

---
+++ POO2024G2-main/SysVentCRUD/src/main/java/pe/edu/upeu/syscenterlife/connx/UtilsX.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.connx;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author Lab-IoT
+ */
+public class UtilsX {
+
+    public File getFileExterno(String carpeta, String filex) {
+        File newFolder = new File(carpeta);
+        String ruta = newFolder.getAbsolutePath();
+        Path CAMINO = Paths.get(ruta + "/" + filex);
+        return CAMINO.toFile();
+    }
+
+}
